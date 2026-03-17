25300144ac7260c7c381c5699c2e44df6c5e5217
nikol4s <nikolassc4mpos@gmail.com>
2026-03-17 19:58:58 -0300

    fix and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -20,11 +20,11 @@ export function Footer() {
             © {currentYear} Todos os direitos reservados.
           </p>
 
-          <div className="flex items-center gap-1 text-sm text-muted-foreground">
+          {/* <div className="flex items-center gap-1 text-sm text-muted-foreground">
             <span>Feito com</span>
             <span className="text-foreground">♥</span>
             <span>e muito café</span>
-          </div>
+          </div> */}
         </div>
       </div>
     </footer>
